e22a0d6c3d8abc7bfbce40e568faf131ea3255ef
sparsons <sparsons@salesforce.com>
2022-02-20 16:43:57 -0500

    reskinning to foodbox

--- foodbox-service-rest/src/main/resources/data.sql
+++ foodbox-service-rest/src/main/resources/data.sql
@@ -1,22 +1,27 @@
-insert into menu(id,name,description,category,imgurl,price) values (1,'Beef Brocoli','A description of a Beef Brocoli.','chinese','assets/img/chinese_beefbrocoli.jpeg',7.99);
-insert into menu(id,name,description,category,imgurl,price) values (10,'Juice','A description of Juice.','chinese','assets/img/',11.79);
-insert into menu(id,name,description,category,imgurl,price) values (11,'Soup','A description of Soup.','chinese','assets/img/',12.89);
-insert into menu(id,name,description,category,imgurl,price) values (12,'Almonds','A description of Almonds.','chinese','assets/img/',17.69);
-insert into menu(id,name,description,category,imgurl,price) values (2,'Broccoli','A description of Broccoli.','chinese','assets/img/chinese_dumpling.jpeg',14.99);
-insert into menu(id,name,description,category,imgurl,price) values (3,'Eggplant','A description of Eggplant.','chinese','assets/img/chinese_eggroll.jpeg',12.99);
-insert into menu(id,name,description,category,imgurl,price) values (4,'Onion','A description of an Onion.','chinese','assets/img/chinese_lomein.jpeg',10.49);
-insert into menu(id,name,description,category,imgurl,price) values (5,'Milk','A description of Milk.','chinese','assets/img/chinese_spicybrocoli.jpeg',14.49);
-insert into menu(id,name,description,category,imgurl,price) values (6,'Steak','A description of Steak.','chinese','assets/img/',11.19);
-insert into menu(id,name,description,category,imgurl,price) values (7,'Chicken','A description of Chicken.','chinese','assets/img/',14.49);
-insert into menu(id,name,description,category,imgurl,price) values (8,'Eggs','A description of Eggs.','chinese','assets/img/',12.49);
-insert into menu(id,name,description,category,imgurl,price) values (9,'Cheese','A description of Cheese.','chinese','assets/img/',12.99);
+insert into menu(id,name,description,category,imgurl,price) values (1,'Sweet &amp; Sour Soup','A description of Sweet &amp; Sour Soup.','chinese','assets/img/chinese_sweetsoursoup.jpeg',7.99);
+insert into menu(id,name,description,category,imgurl,price) values (2,'Dumplings','A description of Dumplings.','chinese','assets/img/chinese_dumpling.jpeg',14.99);
+insert into menu(id,name,description,category,imgurl,price) values (3,'Eggrolls','A description of Eggrolls.','chinese','assets/img/chinese_eggroll.jpeg',12.99);
+insert into menu(id,name,description,category,imgurl,price) values (4,'Lo Mein','A description of Lo Mein.','chinese','assets/img/chinese_lomein.jpeg',10.49);
+insert into menu(id,name,description,category,imgurl,price) values (5,'Spicy Brocoli','A description of Spicy Brocoli.','chinese','assets/img/chinese_spicybrocoli.jpeg',14.49);
+insert into menu(id,name,description,category,imgurl,price) values (6,'Beef Brocoli','A description of Beef Brocoli.','chinese','assets/img/chinese_beefbrocoli.jpeg',11.19);
+
+insert into menu(id,name,description,category,imgurl,price) values (7,'Biryani','A description of Biryani.','indian','assets/img/indian_biryani.jpeg',14.49);
+insert into menu(id,name,description,category,imgurl,price) values (8,'Dosa','A description of Dosa.','indian','assets/img/indian_dosa.jpeg',12.49);
+insert into menu(id,name,description,category,imgurl,price) values (9,'Indian Entree 1','A description of Entree 1.','indian','assets/img/indian_entree1.jpeg',12.99);
+insert into menu(id,name,description,category,imgurl,price) values (10,'Indian Entree 2','A description of Entree 2.','indian','assets/img/indian_entree2.jpeg',11.79);
+insert into menu(id,name,description,category,imgurl,price) values (11,'Indian Entree 3','A description of Entree 3.','indian','assets/img/indian_entreed3.jpeg',12.89);
+insert into menu(id,name,description,category,imgurl,price) values (12,'Indian Entree 4','A description of Entree 4.','indian','assets/img/indian_entree4.jpeg',17.69);
+insert into menu(id,name,description,category,imgurl,price) values (13,'Thali','A description of Thali.','indian','assets/img/indian_thali.jpeg',14.69);
+
+insert into menu(id,name,description,category,imgurl,price) values (14,'Chimichanga','A description of Chimichanga.','mexican','assets/img/mexican_chimichanga.jpeg',14.49);
+insert into menu(id,name,description,category,imgurl,price) values (15,'Fajitas','A description of Fajitas.','mexican','assets/img/mexican_fajitas.jpeg.jpeg',12.49);
+insert into menu(id,name,description,category,imgurl,price) values (16,'Quesadilla Entree','A description of Quesadilla Entree.','mexican','assets/img/mexican_quesadilla.jpeg.jpeg',12.99);
+insert into menu(id,name,description,category,imgurl,price) values (17,'Tacos','A description of Tacos.','mexican','assets/img/mexican_tacos.jpeg',11.79);
+insert into menu(id,name,description,category,imgurl,price) values (18,'Tamale','A description of Tamale.','mexican','assets/img/mexican_tamale.jpeg',12.89);
+
+
 
 insert into user(id,name,address,username,password,adminstatus) values (1,'Sam Parsons','123 Address, Washington, DC, 20010','sam','pw',true);
-insert into user(id,name,address,username,password,adminstatus) values (10,'David Parsons','123 Road, Bloomington, DC, 20010','emi','pw',false);
-insert into user(id,name,address,username,password,adminstatus) values (11,'Sharon Parsons','123 Road, Bloomington, DC, 20010','emi','pw',false);
-insert into user(id,name,address,username,password,adminstatus) values (12,'Aurora Alvarez','123 Calle, Buenos Aries, BS, 20010','suegra','pw',false);
-insert into user(id,name,address,username,password,adminstatus) values (13,'Diego Candia','123 Calle, Viedma, RN, 20010','suegro','pw',false);
-insert into user(id,name,address,username,password,adminstatus) values (14,'Jorge Alvarez','123 Calle, Buenos Aries, BS, 20010','tio','pw',false);
 insert into user(id,name,address,username,password,adminstatus) values (2,'Georgi Candia','123 Street, Washington, DC, 20010','georgi','pw',true);
 insert into user(id,name,address,username,password,adminstatus) values (3,'Puck Parsons','123 Road, Washington, DC, 20010','puck','pw',false);
 insert into user(id,name,address,username,password,adminstatus) values (4,'Ben Parsons','123 Road, Washington, DC, 20010','ben','pw',false);
@@ -25,3 +30,8 @@ insert into user(id,name,address,username,password,adminstatus) values (6,'Milo 
 insert into user(id,name,address,username,password,adminstatus) values (7,'Annika Beczkiewicz','123 Road, Bloomington, DC, 20010','anni','pw',false);
 insert into user(id,name,address,username,password,adminstatus) values (8,'Jana Parsons','123 Road, La Crosse, DC, 20010','jana','pw',false);
 insert into user(id,name,address,username,password,adminstatus) values (9,'Logan Parsons','123 Road, La Crosse, DC, 20010','logie','pw',false);
+insert into user(id,name,address,username,password,adminstatus) values (10,'David Parsons','123 Road, Bloomington, DC, 20010','emi','pw',false);
+insert into user(id,name,address,username,password,adminstatus) values (11,'Sharon Parsons','123 Road, Bloomington, DC, 20010','emi','pw',false);
+insert into user(id,name,address,username,password,adminstatus) values (12,'Aurora Alvarez','123 Calle, Buenos Aries, BS, 20010','suegra','pw',false);
+insert into user(id,name,address,username,password,adminstatus) values (13,'Diego Candia','123 Calle, Viedma, RN, 20010','suegro','pw',false);
+insert into user(id,name,address,username,password,adminstatus) values (14,'Jorge Alvarez','123 Calle, Buenos Aries, BS, 20010','tio','pw',false);
