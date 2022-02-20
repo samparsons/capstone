e55d04321d0b3f8bdcc8cc89bdd8339a49160cde
sparsons <sparsons@salesforce.com>
2022-02-20 17:41:42 -0500

    reskinning to foodbox

--- foodbox-service-rest/src/main/resources/data.sql
+++ foodbox-service-rest/src/main/resources/data.sql
@@ -1,25 +1,23 @@
-insert into menu(id,name,description,category,imgurl,price) values (1,'Sweet &amp; Sour Soup','A description of Sweet &amp; Sour Soup.','chinese','assets/img/chinese_sweetsoursoup.jpeg',7.99);
-insert into menu(id,name,description,category,imgurl,price) values (2,'Dumplings','A description of Dumplings.','chinese','assets/img/chinese_dumpling.jpeg',14.99);
-insert into menu(id,name,description,category,imgurl,price) values (3,'Eggrolls','A description of Eggrolls.','chinese','assets/img/chinese_eggroll.jpeg',12.99);
-insert into menu(id,name,description,category,imgurl,price) values (4,'Lo Mein','A description of Lo Mein.','chinese','assets/img/chinese_lomein.jpeg',10.49);
-insert into menu(id,name,description,category,imgurl,price) values (5,'Spicy Brocoli','A description of Spicy Brocoli.','chinese','assets/img/chinese_spicybrocoli.jpeg',14.49);
-insert into menu(id,name,description,category,imgurl,price) values (6,'Beef Brocoli','A description of Beef Brocoli.','chinese','assets/img/chinese_beefbrocoli.jpeg',11.19);
+insert into menu(id,name,description,category,imgurl,price) values (1,'Sweet &amp; Sour Soup','A description of Sweet &amp; Sour Soup.','chinese','assets/img/chinese/chinese_sweetsoursoup.jpeg',7.99);
+insert into menu(id,name,description,category,imgurl,price) values (2,'Dumplings','A description of Dumplings.','chinese','assets/img/chinese/chinese_dumpling.jpeg',14.99);
+insert into menu(id,name,description,category,imgurl,price) values (3,'Eggrolls','A description of Eggrolls.','chinese','assets/img/chinese/chinese_eggroll.jpeg',12.99);
+insert into menu(id,name,description,category,imgurl,price) values (4,'Lo Mein','A description of Lo Mein.','chinese','assets/img/chinese/chinese_lomein.jpeg',10.49);
+insert into menu(id,name,description,category,imgurl,price) values (5,'Spicy Brocoli','A description of Spicy Brocoli.','chinese','assets/img/chinese/chinese_spicybrocoli.jpeg',14.49);
+insert into menu(id,name,description,category,imgurl,price) values (6,'Beef Brocoli','A description of Beef Brocoli.','chinese','assets/img/chinese/chinese_beefbrocoli.jpeg',11.19);
 
-insert into menu(id,name,description,category,imgurl,price) values (7,'Biryani','A description of Biryani.','indian','assets/img/indian_biryani.jpeg',14.49);
-insert into menu(id,name,description,category,imgurl,price) values (8,'Dosa','A description of Dosa.','indian','assets/img/indian_dosa.jpeg',12.49);
-insert into menu(id,name,description,category,imgurl,price) values (9,'Indian Entree 1','A description of Entree 1.','indian','assets/img/indian_entree1.jpeg',12.99);
-insert into menu(id,name,description,category,imgurl,price) values (10,'Indian Entree 2','A description of Entree 2.','indian','assets/img/indian_entree2.jpeg',11.79);
-insert into menu(id,name,description,category,imgurl,price) values (11,'Indian Entree 3','A description of Entree 3.','indian','assets/img/indian_entreed3.jpeg',12.89);
-insert into menu(id,name,description,category,imgurl,price) values (12,'Indian Entree 4','A description of Entree 4.','indian','assets/img/indian_entree4.jpeg',17.69);
-insert into menu(id,name,description,category,imgurl,price) values (13,'Thali','A description of Thali.','indian','assets/img/indian_thali.jpeg',14.69);
+insert into menu(id,name,description,category,imgurl,price) values (7,'Biryani','A description of Biryani.','indian','assets/img/indian/indian_biryani.jpeg',14.49);
+insert into menu(id,name,description,category,imgurl,price) values (8,'Dosa','A description of Dosa.','indian','assets/img/indian/indian_dosa.jpeg',12.49);
+insert into menu(id,name,description,category,imgurl,price) values (9,'Indian Entree 1','A description of Entree 1.','indian','assets/img/indian/indian_entree1.jpeg',12.99);
+insert into menu(id,name,description,category,imgurl,price) values (10,'Indian Entree 2','A description of Entree 2.','indian','assets/img/indian/indian_entree2.jpeg',11.79);
+insert into menu(id,name,description,category,imgurl,price) values (11,'Indian Entree 3','A description of Entree 3.','indian','assets/img/indian/indian_entreed3.jpeg',12.89);
+insert into menu(id,name,description,category,imgurl,price) values (12,'Indian Entree 4','A description of Entree 4.','indian','assets/img/indian/indian_entree4.jpeg',17.69);
+insert into menu(id,name,description,category,imgurl,price) values (13,'Thali','A description of Thali.','indian','assets/img/indian/indian_thali.jpeg',14.69);
 
-insert into menu(id,name,description,category,imgurl,price) values (14,'Chimichanga','A description of Chimichanga.','mexican','assets/img/mexican_chimichanga.jpeg',14.49);
-insert into menu(id,name,description,category,imgurl,price) values (15,'Fajitas','A description of Fajitas.','mexican','assets/img/mexican_fajitas.jpeg.jpeg',12.49);
-insert into menu(id,name,description,category,imgurl,price) values (16,'Quesadilla Entree','A description of Quesadilla Entree.','mexican','assets/img/mexican_quesadilla.jpeg.jpeg',12.99);
-insert into menu(id,name,description,category,imgurl,price) values (17,'Tacos','A description of Tacos.','mexican','assets/img/mexican_tacos.jpeg',11.79);
-insert into menu(id,name,description,category,imgurl,price) values (18,'Tamale','A description of Tamale.','mexican','assets/img/mexican_tamale.jpeg',12.89);
-
-
+insert into menu(id,name,description,category,imgurl,price) values (14,'Chimichanga','A description of Chimichanga.','mexican','assets/img/mexican/mexican_chimichanga.jpeg',14.49);
+insert into menu(id,name,description,category,imgurl,price) values (15,'Fajitas','A description of Fajitas.','mexican','assets/img/mexican/mexican_fajitas.jpeg.jpeg',12.49);
+insert into menu(id,name,description,category,imgurl,price) values (16,'Quesadilla Entree','A description of Quesadilla Entree.','mexican','assets/img/mexican/mexican_quesadilla.jpeg.jpeg',12.99);
+insert into menu(id,name,description,category,imgurl,price) values (17,'Tacos','A description of Tacos.','mexican','assets/img/mexican/mexican_tacos.jpeg',11.79);
+insert into menu(id,name,description,category,imgurl,price) values (18,'Tamale','A description of Tamale.','mexican','assets/img/mexican/mexican_tamale.jpeg',12.89);
 
 insert into user(id,name,address,username,password,adminstatus) values (1,'Sam Parsons','123 Address, Washington, DC, 20010','sam','pw',true);
 insert into user(id,name,address,username,password,adminstatus) values (2,'Georgi Candia','123 Street, Washington, DC, 20010','georgi','pw',true);
